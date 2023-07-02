0e7063aa46e418ae244179c5f227b4ccf9609d32
Janos Hardi <yaanno@gmail.com>
2023-07-02 12:15:13 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use http_cache_reqwest::{CACacheManager, Cache, CacheMode, HttpCache};
+use reqwest::Client;
+use reqwest_middleware::{ClientBuilder, Result};
+
+#[path = "lib.rs"]
+mod lib;
+use lib::Results;
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    let client = ClientBuilder::new(Client::new())
+        .with(Cache(HttpCache {
+            mode: CacheMode::Default,
+            manager: CACacheManager::default(),
+            options: None,
+        }))
+        .build();
+
+    let shows: Results = client
+    .get("https://api.tvmaze.com/schedule/web?date=2023-07-02")
+    .send()
+    .await?
+    .json()
+    .await?;
+
+    println!("{:#?}", shows.first());
+    Ok(())
+}
